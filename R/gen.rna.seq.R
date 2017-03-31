ea76702ee0abd75328cd44f485963507fb42b96d
André Veríssimo <afsverissimo@gmail.com>
2017-03-31 17:19:49 +0100

    small corrections on package and generate vignette

--- R/gen.rna.seq.R
+++ R/gen.rna.seq.R
@@ -14,12 +14,12 @@
 #' gen.rna.seq(100)
 #' gen.rna.seq(10, log(1.2), log(1 + 0.5))
 gen.rna.seq <- function(len,
-                        meanlog = log(runif(1, min = 0, max= 3)),
-                        sdlog   = log(1 + runif(1, min = 0.08, max= 1.5))) {
-  res <- rlnorm(len, meanlog = meanlog, sdlog = sdlog)
+                        meanlog = log(stats::runif(1, min = 0, max= 3)),
+                        sdlog   = log(1 + stats::runif(1, min = 0.08, max= 1.5))) {
+  res <- stats::rlnorm(len, meanlog = meanlog, sdlog = sdlog)
   if (meanlog <= log(1e-1)) {
     # higher change if low expressed genes
-    res[runif(length(res)) >= 0.9] <- 0
+    res[stats::runif(length(res)) >= 0.9] <- 0
     sdlog <- log(1) + sdlog / 10
   }
   return(res)
